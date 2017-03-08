f623f33f9a82d2be8ea92deba2c582be3fdba2f5
John Muschelli <muschellij2@gmail.com>
2017-03-08 18:59:33 +0000

    version 2.2.2

--- R/download_img_data.R
+++ R/download_img_data.R
@@ -1,22 +1,49 @@
 #' @title Download T1 and T2 data
 #' @description Download T1 and T2 data for Examples
 #' @return Logical indicator if the files were downloaded.
+#' @param lib.loc a character vector with path names of R libraries. 
+#' Passed to \code{\link{img_data}} 
 #' @export
-download_img_data = function(){
+download_img_data = function(lib.loc = NULL){
+  stubs = c("T1Strip.nii.gz",
+            "T2Strip.nii.gz")  
+  img_files = ws_img_data(lib.loc = lib.loc,
+                          warn = FALSE)
+  
+  if (!all(file.exists(img_files))) {
+    for (istub in stubs) {
+      url = paste0("http://muschellij2.github.io/WhiteStripe/", istub)
+      urlfile <- file.path(system.file(package = "WhiteStripe",
+                                       lib.loc = lib.loc), 
+                         istub)
+      download.file(url, urlfile, quiet = TRUE)
+    }
+  }
+  img_files = ws_img_data(lib.loc = lib.loc,
+                          warn = FALSE)
+
+  return(all(file.exists(img_files)))
+}
+
+#' @title Return Filenames of T1 and T2 data
+#' @description Return filenames T1 and T2 data for example 
+#' and vignettes
+#' @return Vector of filenames
+#' @param lib.loc a character vector with path names of R libraries. 
+#' Passed to \code{\link{system.file}} 
+#' @param warn Should a warning be printed if the images were not
+#' there
+#' @export
+ws_img_data = function(lib.loc = NULL, warn = TRUE){
   stubs = c("T1Strip.nii.gz",
             "T2Strip.nii.gz")
   img_files = system.file(stubs,
-                          package="WhiteStripe")
-  
-  if (!all(file.exists(img_files))){
-    for (istub in stubs){
-      url = paste0("http://muschellij2.github.io/WhiteStripe/", istub)
-      urlfile <- file.path(system.file(package="WhiteStripe"), 
-                         istub)
-      download.file(url, urlfile, quiet=TRUE)
+                          package = "WhiteStripe",
+                          lib.loc = lib.loc)
+  if (all(img_files == "")) {
+    if (warn) {
+      warning("Files are not downloaded, use download_img_data first!")
     }
   }
-  img_files = system.file(stubs,
-                          package="WhiteStripe")
-  return(all(file.exists(img_files)))
+  return(img_files)
 }
---
+++ inst/doc/Running_WhiteStripe.R
@@ -0,0 +1,40 @@
+## ----dl_data-------------------------------------------------------------
+library(WhiteStripe)
+download_img_data()
+
+## ----get_data------------------------------------------------------------
+files = ws_img_data()
+
+## ----t1------------------------------------------------------------------
+library(oro.nifti)
+t1 = files[grep("T1", basename(files))]
+img = readNIfTI(fname = t1, reorient = FALSE)
+
+## ------------------------------------------------------------------------
+orthographic(img)
+
+## ----hist----------------------------------------------------------------
+vals = img[img > 0]
+hist(vals, breaks = 2000)
+
+## ----ws------------------------------------------------------------------
+ws = whitestripe(img = img, type = "T1", stripped = TRUE)
+names(ws)
+
+## ----norm_ws-------------------------------------------------------------
+norm = whitestripe_norm(img = img, indices = ws$whitestripe.ind)
+
+## ----print_ws------------------------------------------------------------
+hist(vals, breaks = 2000)
+abline(v = ws$mu.whitestripe, col = "blue")
+abline(v = ws$whitestripe, col = "red")
+
+## ----ortho_overlay-------------------------------------------------------
+mask = ws$mask.img
+mask[mask == 0] = NA
+orthographic(x = img, y = mask, col.y = "red")
+
+## ----norm_hist-----------------------------------------------------------
+norm_vals = norm[img > 0]
+hist(norm_vals, breaks = 2000)
+
