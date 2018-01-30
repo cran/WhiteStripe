03417eda85a8d69c62a4beff0c1cba328f3f22b9
John Muschelli <muschellij2@gmail.com>
2018-01-30 10:35:53 +0000

    version 2.3.0

---
+++ NEWS.md
@@ -0,0 +1,9 @@
+# WhiteStripe 2.3.0
+
+* Fixed the installation to not write user directory for Debian.
+
+* Aded FA and MD operations for whitestripe.
+
+* Added a `NEWS.md` file to track changes to the package.
+
+
--- R/whitestripe_ind.R
+++ R/whitestripe_ind.R
@@ -35,8 +35,8 @@ make_img_voi = function(img, slices = 80:120, na.rm = TRUE, ...){
 #'
 #' @description Returns the mean/sd of the whitestripe and indices
 #' for them on the image
-#' @param img Image (T1 or T2).  Array or object of class nifti
-#' @param type T1 or T2 image whitestripe
+#' @param img Image (T1, T2, FA, or MD).  Array or object of class nifti
+#' @param type T1, T2, FA, or MD image whitestripe
 #' @param breaks Number of breaks passed to \code{\link{hist}}
 #' @param whitestripe.width Radius of the white stripe
 #' @param whitestripe.width.l Lower Radius of the white stripe
@@ -60,13 +60,23 @@ make_img_voi = function(img, slices = 80:120, na.rm = TRUE, ...){
 #' will not rerun \code{\link{make_img_voi}}.
 #' @examples
 #' \dontrun{
-#' if (download_img_data()){
-#' t1 = readNIfTI(system.file("T1Strip.nii.gz", package="WhiteStripe"))
+#' library(WhiteStripe)
+#' lib.loc = tempdir()
+#' if (WhiteStripe::download_img_data(lib.loc = lib.loc)){
+#' library(oro.nifti)
+#' set.seed(1)
+#' t1 = readNIfTI(system.file("T1Strip.nii.gz", package="WhiteStripe",
+#' lib.loc = lib.loc))
 #' t1.ind = whitestripe(t1, "T1")
+#' set.seed(2)
+#' t1_2 = readNIfTI(system.file("T1Strip.nii.gz", package="WhiteStripe",
+#' lib.loc = lib.loc))
+#' t1_2.ind = whitestripe(t1_2, "T1") 
 #' t1.mask = whitestripe_ind_to_mask(t1, t1.ind$whitestripe.ind)
 #' t1.mask[t1.mask == 0] = NA
 #' orthographic(t1, t1.mask, col.y="red")
-#' t2 = readNIfTI(system.file("T2Strip.nii.gz", package="WhiteStripe"))
+#' t2 = readNIfTI(system.file("T2Strip.nii.gz", package="WhiteStripe",
+#' lib.loc = lib.loc))
 #' t2.ind = whitestripe(t2, "T2")
 #' t2.mask = whitestripe_ind_to_mask(t2, t2.ind$whitestripe.ind)
 #' t2.mask[t2.mask == 0] = NA
@@ -78,81 +88,85 @@ make_img_voi = function(img, slices = 80:120, na.rm = TRUE, ...){
 #' @importFrom graphics hist
 whitestripe <- function(
   img,
-  type = c("T1", "T2", "last", "largest"),
+  type = c("T1", "T2", "FA", "MD", "first", "last", "largest"),
   breaks = 2000, whitestripe.width = 0.05,
   whitestripe.width.l = whitestripe.width,
   whitestripe.width.u = whitestripe.width,
   arr.ind = FALSE, verbose = TRUE,
   stripped = FALSE, slices = NULL, ...)  {
-    if (verbose) {
-        message(paste0("Making Image VOI\n"))
+  if (verbose) {
+    message(paste0("Making Image VOI\n"))
+  }
+  if (stripped) {
+    img.voi <- img[img > 0]
+  } else {
+    is_voi = inherits(img, "img_voi")
+    if (is.null(slices) & !is_voi) {
+      message(
+        paste0("Using all slices of the image as slices not defined!", 
+               " Use stripped = TRUE if using skull-stripped images.")
+      )        
+      d3 = dim(as(img, "array"))[3]
+      slices = seq(d3)        
     }
-    if (stripped) {
-        img.voi <- img[img > 0]
-    } else {
-      is_voi = inherits(img, "img_voi")
-      if (is.null(slices) & !is_voi) {
-        message(
-          paste0("Using all slices of the image as slices not defined!", 
-                 " Use stripped = TRUE if using skull-stripped images.")
-        )        
-        d3 = dim(as(img, "array"))[3]
-        slices = seq(d3)        
-      }
-      img.voi <- make_img_voi(img, slices = slices, ...)
-    }
-    if (verbose) {
-        message(paste0("Making ", type, " Histogram\n"))
-    }
-    img.hist = hist(img.voi, breaks = breaks, plot = FALSE)
-    y.in = img.hist$counts
-    x.in = img.hist$mids
-    x.in = x.in[!is.na(y.in)]
-    y.in = y.in[!is.na(y.in)]
-    type = match.arg(type)
-    stopifnot(length(type) == 1)
-    if (verbose) {
-        cat(paste0("Getting ", type, " Modes\n"))
-    }
-    if (type %in% c("T1", "last")) {
-        img.mode = get.last.mode(x.in, y.in, verbose = verbose,
-            ...)
-    }
-    if (type %in% c("T2", "largest")) {
-        img.mode = get.largest.mode(x.in, y.in, verbose = verbose,
-            ...)
-    }
-    img.mode.q = mean(img.voi < img.mode)
-    if (verbose) {
-        cat(paste0("Quantile ", type, " VOI\n"))
-    }
-    whitestripe = quantile(img.voi, probs = c(max(img.mode.q -
-        whitestripe.width.l, 0), min(img.mode.q + whitestripe.width.u,
-        1)), na.rm = TRUE)
-    whitestripe.ind = which((img > whitestripe[1]) & (img < whitestripe[2]),
-        arr.ind = arr.ind)
-    err = FALSE
-    if (length(whitestripe.ind) == 0) {
-        warning(paste0("Length of White Stripe is 0 for ", type,
-            ", using whole brain normalization"))
-        whitestripe.ind = which(img > mean(img))
-        err = TRUE
-    }
-    mu.whitestripe = img.mode
-    sig.whitestripe = sd(img[whitestripe.ind])
-    mask.img = img
-    mask.img[!is.na(mask.img) | is.na(mask.img)] = 0
-    mask.img[whitestripe.ind] = 1
-    if (inherits(img, "nifti")) {
-        mask.img = cal_img(mask.img)
-        mask.img = zero_trans(mask.img)
-    }
-    return(list(whitestripe.ind = whitestripe.ind, img.mode = img.mode,
-        mask.img = mask.img, mu.whitestripe = mu.whitestripe,
-        sig.whitestripe = sig.whitestripe, img.mode.q = img.mode.q,
-        whitestripe = whitestripe, whitestripe.width = whitestripe.width,
-        whitestripe.width.l = whitestripe.width.l, whitestripe.width.u = whitestripe.width.u,
-        err = err))
+    img.voi <- make_img_voi(img, slices = slices, ...)
+  }
+  if (verbose) {
+    message(paste0("Making ", type, " Histogram\n"))
+  }
+  img.hist = hist(img.voi, breaks = breaks, plot = FALSE)
+  y.in = img.hist$counts
+  x.in = img.hist$mids
+  x.in = x.in[!is.na(y.in)]
+  y.in = y.in[!is.na(y.in)]
+  type = match.arg(type)
+  stopifnot(length(type) == 1)
+  if (verbose) {
+    cat(paste0("Getting ", type, " Modes\n"))
+  }
+  if (type %in% c("T1", "FA", "last")) {
+    img.mode = get.last.mode(x.in, y.in, verbose = verbose,
+                             ...)
+  }
+  if (type %in% c("T2", "largest")) {
+    img.mode = get.largest.mode(x.in, y.in, verbose = verbose,
+                                ...)
+  }
+  if (type %in% c("MD", "first")) {
+    img.mode = get.first.mode(x.in, y.in, verbose = verbose,
+                              ...)
+  }    
+  img.mode.q = mean(img.voi < img.mode)
+  if (verbose) {
+    cat(paste0("Quantile ", type, " VOI\n"))
+  }
+  whitestripe = quantile(img.voi, probs = c(max(img.mode.q -
+                                                  whitestripe.width.l, 0), min(img.mode.q + whitestripe.width.u,
+                                                                               1)), na.rm = TRUE)
+  whitestripe.ind = which((img > whitestripe[1]) & (img < whitestripe[2]),
+                          arr.ind = arr.ind)
+  err = FALSE
+  if (length(whitestripe.ind) == 0) {
+    warning(paste0("Length of White Stripe is 0 for ", type,
+                   ", using whole brain normalization"))
+    whitestripe.ind = which(img > mean(img))
+    err = TRUE
+  }
+  mu.whitestripe = img.mode
+  sig.whitestripe = sd(img[whitestripe.ind])
+  mask.img = img
+  mask.img[!is.na(mask.img) | is.na(mask.img)] = 0
+  mask.img[whitestripe.ind] = 1
+  if (inherits(img, "nifti")) {
+    mask.img = cal_img(mask.img)
+    mask.img = zero_trans(mask.img)
+  }
+  return(list(whitestripe.ind = whitestripe.ind, img.mode = img.mode,
+              mask.img = mask.img, mu.whitestripe = mu.whitestripe,
+              sig.whitestripe = sig.whitestripe, img.mode.q = img.mode.q,
+              whitestripe = whitestripe, whitestripe.width = whitestripe.width,
+              whitestripe.width.l = whitestripe.width.l, whitestripe.width.u = whitestripe.width.u,
+              err = err))
 }
 
 
@@ -166,7 +180,7 @@ whitestripe <- function(
 #' Can also be a mask (indices where mask > 0 are used.)
 #' @param ... arguments to be passed to \code{\link{mean}} and \code{\link{sd}}
 #' @export
-#' @return Object of same class as img, but normalized
+#' @return Object of same class as \code{img}, but normalized
 whitestripe_norm = function(img, indices, ...){
   if (inherits(indices, "nifti")){
     indices = which(indices > 0)
@@ -195,9 +209,12 @@ whitestripe_norm = function(img, indices, ...){
 #' @aliases hybrid
 #' @examples
 #' \dontrun{
-#' if (download_img_data()){
-#' t1 = readNIfTI(system.file("T1Strip.nii.gz", package="WhiteStripe"))
-#' t2 = readNIfTI(system.file("T2Strip.nii.gz", package="WhiteStripe"))
+#' lib.loc = tempdir()
+#' if (download_img_data(lib.loc = lib.loc)){
+#' t1 = readNIfTI(system.file("T1Strip.nii.gz", package="WhiteStripe", 
+#' lib.loc = lib.loc))
+#' t2 = readNIfTI(system.file("T2Strip.nii.gz", package="WhiteStripe",
+#' lib.loc = lib.loc))
 #' ind = whitestripe_hybrid(t1, t2)
 #' }
 #'}
@@ -205,7 +222,7 @@ whitestripe_hybrid = function(t1, t2, ...){
   t1.ws = whitestripe(t1, type="T1", ...)
   t2.ws = whitestripe(t2, type="T2", ...)
   whitestripe.ind = intersect(t1.ws$whitestripe.ind,
-    t2.ws$whitestripe.ind)
+                              t2.ws$whitestripe.ind)
   mask.img = t1
   mask.img[!is.na(mask.img) | is.na(mask.img)] = 0
   mask.img[whitestripe.ind] = 1
@@ -230,16 +247,20 @@ whitestripe_hybrid = function(t1, t2, ...){
 #' @export
 #' @keywords hybrid, whitestripe
 #' @seealso whitestripe, whitestripe_hybrid
-#' @return Class of array or nifti depending on img input
+#' @return Class of array or nifti depending on \code{img} input
 #' @aliases whitemask
 #' @importFrom oro.nifti writeNIfTI
 #' @importFrom oro.nifti cal_img
 #' @importFrom oro.nifti zero_trans
 #' @examples
 #' \dontrun{
-#' if (download_img_data()){
-#' t1 = readNIfTI(system.file("T1Strip.nii.gz", package="WhiteStripe"))
-#' t2 = readNIfTI(system.file("T2Strip.nii.gz", package="WhiteStripe"))
+#' lib.loc = tempdir()
+#' 
+#' if (download_img_data(lib.loc = lib.loc)){
+#' t1 = readNIfTI(system.file("T1Strip.nii.gz", package="WhiteStripe",
+#' lib.loc = lib.loc))
+#' t2 = readNIfTI(system.file("T2Strip.nii.gz", package="WhiteStripe",
+#' lib.loc = lib.loc))
 #' ind = whitestripe_hybrid(t1, t2)
 #' mask = whitestripe_ind_to_mask(t1, ind$whitestripe.ind)
 #' orthographic(mask)
@@ -255,7 +276,7 @@ whitestripe_ind_to_mask = function(img, indices, writeimg=FALSE, ...){
       writeNIfTI(nim=img, ...)
     }
   }
-
+  
   return(img)
 }
 
